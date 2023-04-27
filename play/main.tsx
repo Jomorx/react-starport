dcde338a8d14b88368ee0533eb6f7e848b34a933
huchanghao <2955568032@qq.com>
2023-04-27 12:47:23 +0800

    change hash

--- play/main.tsx
+++ play/main.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import StarportScope from "../packages/StarPort/components/StarportScope";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   // <React.StrictMode>
-  <BrowserRouter>
+  <HashRouter>
     <StarportScope>
         <App />
     </StarportScope>
-  </BrowserRouter>
+  </HashRouter>
   // </React.StrictMode>
 );
 
